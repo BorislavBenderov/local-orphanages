120c4c89b449214f210527ed09aef2515cbb1966
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-10-20 17:22:21 +0300

    Make basic router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
+import { Routes, Route } from 'react-router-dom';
+
 import { Header } from "./components/Header/Header";
 import { Home } from "./components/Home/Home";
+import { Create } from "./components/Create/Create";
+import { Login } from "./components/Login/Login";
+import { Register } from "./components/Register/Register";
+import { Logout } from "./components/Logout/Logout";
+import { MyPosts } from "./components/MyPosts/MyPosts";
+import { Details } from "./components/Details/Details";
+import { Edit } from "./components/Edit/Edit";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <main id="main-content">
-        <Home />
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/create' element={<Create />}/>
+          <Route path='/login' element={<Login />}/>
+          <Route path='/register' element={<Register />}/>
+          <Route path='/logout' element={<Logout />}/>
+          <Route path='/myposts' element={<MyPosts />}/>
+          <Route path='/details/:postId' element={<Details />}/>
+          <Route path='/edit/:postId' element={<Edit />}/>
+        </Routes>
       </main>
     </div>
   );
