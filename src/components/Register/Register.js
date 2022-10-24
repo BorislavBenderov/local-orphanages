b00276eb8d572b44c4c298ae359d0c439229fc22
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-10-24 16:07:39 +0300

    Make register component to work

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,7 +1,36 @@
+import * as authService from '../../services/authService';
+import { useContext } from 'react';
+import { AuthContext } from '../../contexts/AuthContext';
+import { useNavigate } from 'react-router-dom';
+
 export const Register = () => {
+    const { onLogin } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        const formdata = new FormData(e.target);
+
+        const email = formdata.get('email');
+        const password = formdata.get('password');
+        const repeatPassword = formdata.get('repeatPassword');
+
+        if (password !== repeatPassword) {
+            return;
+        }
+
+        authService.register(email, password)
+        .then(userdata => {
+            onLogin(userdata);
+            navigate('/');
+        })
+
+    }
+
     return (
         <section id="register-page" className="auth">
-            <form id="register">
+            <form id="register" onSubmit={onSubmit}>
                 <h1 className="title">Register</h1>
                 <article className="input-group">
                     <label htmlFor="register-email">Email: </label>
