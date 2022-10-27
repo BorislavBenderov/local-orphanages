74287920525671f276c722a32fa332acc28b483f
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-10-27 14:19:12 +0300

    Make myposts component show owner posts

--- src/components/MyPosts/MyPosts.js
+++ src/components/MyPosts/MyPosts.js
@@ -1,16 +1,19 @@
 import { useContext } from "react";
 import { PostContext } from '../../contexts/PostContext';
+import { AuthContext } from "../../contexts/AuthContext";
 import { Post } from "./Post";
 
 export const MyPosts = () => {
     const { posts } = useContext(PostContext);
+    const { auth } = useContext(AuthContext);
+    const ownerPosts = posts.filter(x => x._ownerId === auth._id);
 
     return (
         <section id="my-posts-page">
             <h1 className="title">My Posts</h1>
             <div className="my-posts">
-                {posts.length > 0
-                    ? posts.map(post => <Post key={post._id} post={post} />)
+                {ownerPosts.length > 0
+                    ? ownerPosts.map(post => <Post key={post._id} post={post} />)
                     : <h1 className="title no-posts-title">You have no posts yet!</h1>}
             </div>
         </section>
